c6fc808f9122e8772762b74773f0cce1e78218b0
sharifrashed <sharif.rashed26@gmail.com>
2021-12-10 15:55:24 -0600

    started to work on setup

---
+++ src/scripts/Buttons.js
@@ -0,0 +1,28 @@
+import { sendReservation } from "./dataAccess.js"
+
+export const ReservationForm = () => {
+    let html = `
+    <div class="field">
+    <label class="label" for="serviceDescription">Description</label>
+    <input type="text" name="serviceDescription" class="input" />
+    </div>
+    <div class="field">
+    <label class="label" for="serviceAddress">Address</label>
+    <input type="text" name="serviceAddress" class="input" />
+    </div>
+    <div class="field">
+    <label class="label" for="serviceBudget">Budget</label>
+    <input type="number" name="serviceBudget" class="input" />
+    </div>
+    <div class="field">
+    <label class="label" for="serviceDate">Date needed</label>
+    <input type="date" name="serviceDate" class="input" />
+    </div>
+    
+    <div class="requestButton">
+    <button class="button" id="submitRequest">Submit Request</button>
+    </div>
+    `
+
+    return html
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+import { Buttons } from "./Buttons.js"
+import { fetchReservations } from "./dataAccess.js"
+
+const mainContainer = document.querySelector("#container")
+
+const render = () => {
+    fetchReservations().then(
+        () => {
+            mainContainer.innerHTML = Buttons()
+        }
+    )
+}
+
+render()
