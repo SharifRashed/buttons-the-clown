e5f936bcfd5eda609d2f11daae3fd966562a974f
sharifrashed <sharif.rashed26@gmail.com>
2021-12-13 12:02:50 -0600

    feat: completed everything to work properly except one evevent listener

---
+++ src/scripts/Requests.js
@@ -0,0 +1,34 @@
+// this module is responsible for the reservation forms a customer uses to submit a reservation for a clown
+
+import { getRequests } from "./dataAccess.js"
+import { deleteRequest } from "./dataAccess.js"
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [, requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
+
+export const Requests = () => {
+    const requests = getRequests()
+    const listItemsArray = (request) => {
+
+        return `<li class="request__delete">${request.parentName}<button
+                id="request--${request.id}">
+                delete
+        </button></li>`
+    }
+
+
+    let html = `
+        <ul>
+        ${requests.map(listItemsArray).join("")
+        }
+      
+        </ul>
+    `
+
+    return html
+}
